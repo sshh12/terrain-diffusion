374c00fc40987caf13c7b4bb893564bfb9c842dc
Shrivu Shankar <shrivu1122@gmail.com>
2023-08-06 17:38:27 -0700

    tweaks

---
+++ scripts/build_text2rgb_dataset.py
@@ -0,0 +1,33 @@
+import argparse
+import json
+import tqdm
+import os
+
+from PIL import Image
+
+
+def main(caption_path, dataset_path):
+    train_dir = os.path.join(dataset_path, "train", "images")
+    os.makedirs(dataset_path, exist_ok=True)
+    os.makedirs(train_dir, exist_ok=True)
+
+    id_ = 0
+    with open(os.path.join(train_dir, "metadata.jsonl"), "w") as metacsv:
+        with open(caption_path, "r") as f:
+            for line in tqdm.tqdm(f):
+                data = json.loads(line)
+                img = Image.open(data["rgb_fn"])
+                save_fn = f"{id_:06d}.png"
+                img.save(os.path.join(train_dir, save_fn))
+
+                meta = dict(file_name=save_fn, text=data["caption"])
+                metacsv.write(f"{json.dumps(meta)}\n")
+                id_ += 1
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--caption_path", type=str)
+    parser.add_argument("--dataset_path", type=str)
+    args = parser.parse_args()
+    main(args.caption_path, args.dataset_path)
